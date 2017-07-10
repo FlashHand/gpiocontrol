5346d055ec9639c87abdb46d750ee063f6815c73
BoWang <ralwayne@163.com>
2017-07-10 15:48:02 +0800

    nm

--- gpio/control.js
+++ gpio/control.js
@@ -1,12 +1,54 @@
 /**
  * Created by R4L on 2017/7/10.
  */
+let waterfall = require('async/waterfall');
 var gpio = require("gpio");
-
+let pins = [];
 // Calling export with a pin number will export that header and return a gpio header instance
-var gpio16 = gpio.export(16, {
-  interval: 200,
-  ready: function() {
-    gpio16.set();
+waterfall([
+  function (cb) {
+    var gpio16 = gpio.export(16, {
+      interval: 200,
+      ready: function () {
+        pins.push(gpio16);
+        cb(null);
+      }
+    });
+  },
+  function (cb) {
+    var gpio18 = gpio.export(18, {
+      interval: 200,
+      ready: function () {
+        pins.push(gpio18);
+        cb(null);
+      }
+    });
+  },
+  function (cb) {
+    var gpio22 = gpio.export(22, {
+      interval: 200,
+      ready: function () {
+        pins.push(gpio22);
+        cb(null);
+      }
+    });
+  },
+  function (cb) {
+    var gpio36 = gpio.export(36, {
+      interval: 200,
+      ready: function () {
+        pins.push(gpio36);
+        cb(null);
+      }
+    });
   }
+], function (err, result) {
+  // result now equals 'done'
+  startBlin();
 });
+let level = 0;
+let startBlin = ()=> {
+  setInterval(()=> {
+    pins[3].set(1-level);
+  }, 1000)
+}
